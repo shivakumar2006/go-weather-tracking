f784e24a20c926c2848b11e15782b8aff26fc121
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-18 05:01:58 +0530

    solved

--- main.go
+++ main.go
@@ -11,3 +11,25 @@
 
 // }
 
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	text := "Hello world! This is a simple world of Programming"
+
+	if strings.Contains(text, "world") {
+		fmt.Println("world was contain...")
+	} else {
+		fmt.Println("world was not contain...")
+	}
+
+	words := strings.Split(text, "  ")
+	fmt.Println("Words in the text : ", words)
+
+	replacedText := strings.Replace(text, "world", "GO-lang", -1)
+	fmt.Println("text after replacement", replacedText)
+}
