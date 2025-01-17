0fc5aa0d3931876b76339eb15873ae5b16b400c3
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-18 03:58:43 +0530

    this is how to encode and decode in go

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"strings"
+// )
+
+// func main() {
+
+// }
+
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	person := Person{
+		Name: "Shiva",
+		Age:  18,
+	}
+
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("Error marshalling JSON: ", err)
+	}
+
+	fmt.Println("JSON data : ", string(jsonData))
+
+	var p Person
+	err = json.Unmarshal(jsonData, &p)
+	if err != nil {
+		fmt.Println("Error unmarshalling data : ", err)
+		return
+	}
+
+	fmt.Printf("Unmarshalled struct, %+v \n", p)
+	fmt.Println("Name", p.Name)
+	fmt.Println("Age", p.Age)
 }
