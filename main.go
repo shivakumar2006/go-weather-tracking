6f958971ac377ee1b646546c1986139508f1b8b1
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-18 13:56:36 +0530

    complete project

--- main.go
+++ main.go
@@ -1,35 +1,75 @@
-// package main
-
-// import (
-// 	"encoding/json"
-// 	"io/ioutil"
-// 	"net/http"
-// 	"strings"
-// )
-
-// func main() {
-
-// }
-
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"strings"
 )
 
-func main() {
-	text := "Hello world! This is a simple world of Programming"
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
 
-	if strings.Contains(text, "world") {
-		fmt.Println("world was contain...")
-	} else {
-		fmt.Println("world was not contain...")
+type WeatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Celcius float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return apiConfigData{}, err
 	}
 
-	words := strings.Split(text, "  ")
-	fmt.Println("Words in the text : ", words)
+	var c apiConfigData
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	return c, nil
+}
 
-	replacedText := strings.Replace(text, "world", "GO-lang", -1)
-	fmt.Println("text after replacement", replacedText)
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go! \n"))
+}
+
+func query(city string) (WeatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return WeatherData{}, err
+	}
+
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return WeatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d WeatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return WeatherData{}, err
+	}
+	return d, nil
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json: charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8080", nil)
 }
