0b4a0377f56123a80d2a59d35d68d3f4b84e4ff1
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-19 03:15:05 +0530

    commit

--- frontend/src/Weather.js
+++ frontend/src/Weather.js
@@ -7,16 +7,25 @@ const Weather = () => {
     const [ city, setCity ] = useState('');
     const [ error, setError ] = useState('');
     const [ loading, setLoading ] = useState('');
+    const [ weatherData, setWeatherData ] = useState('');
 
-    const fetchWeather = () => {
+    const fetchWeather = async () => {
         if (!city) {
             setError("Enter a city name...");
             return 
         }
         setLoading(true);
-        try
-
-    }
+        try {
+            const response = await axios.get(`http://localhost:8080/weather/${city}`);
+            setWeatherData(response.data);
+            setError('');
+        } catch (err) {
+            setError("Unable to fetch weather data. Please check the city name...");
+            setWeatherData(null);
+        } finally {
+            setLoading(false);
+        };
+    };
 
     return (
         <div className='container'>
@@ -32,6 +41,16 @@ const Weather = () => {
             </div> 
             {loading && <p>Loading...</p>}
             {error && <p className="error">{error}</p>}
+            {weatherData && (
+                <div className='weather-info'>
+                    <h2>{weatherData.name}</h2>
+                    <p>Temperature: {}</p>
+                    <p>Humidity: {}</p>
+                    <p>Pressure: {}</p>
+                    <p>Wind Speed: {}</p>
+                    <p>Description: {}</p>
+                </div>
+            )}
         </div>
     )
 }
